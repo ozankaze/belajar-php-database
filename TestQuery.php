726ea1ea48a109267c6e70cf230f14b729551cd0
ozankaze <ozankaze101@gmail.com>
2023-04-14 04:12:39 +0700

    PHP MySQL Database Query SQL

---
+++ TestQuery.php
@@ -0,0 +1,23 @@
+<?php
+
+// 23
+
+require_once __DIR__ . "/GetConnection.php";
+
+$connection = getConnection();
+
+$sql = "SELECT id, name, email FROM customers";
+$results = $connection->query($sql);
+
+foreach ($results as $result) {
+    // var_dump($result);
+    $id = $result[0];
+    $name = $result["name"];
+    $email = $result["email"];
+
+    echo "Id: $id" . PHP_EOL;
+    echo "Name: $name" . PHP_EOL;
+    echo "Email: $email" . PHP_EOL;
+}
+
+$connection = null;
